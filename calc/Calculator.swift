d7501592e00e02bd429086b254312a9e8f22da91
Blockmaster2118 <49419708+Blockmaster2118@users.noreply.github.com>
2025-03-13 19:00:16 +1100

    code test

--- calc/Calculator.swift
+++ calc/Calculator.swift
@@ -28,10 +28,161 @@ class Calculator {
     }
     
     func calculate(args: [String]) -> String {
-        // Todo: Calculate Result from the arguments. Replace dummyResult with your actual result;
-        let dummyResult = add(no1: 1, no2: 2);
-        
-        let result = String(dummyResult);
+        let result = String((evaluate(args)));
         return(result)
+    }
+    
+    let pattern = "^(?:-?[0-9]+|\\[-?[0-9]+|-?[0-9]+\\]|[+\\-x/%])$"
+
+    func precedence(_ op: String) -> Int {
+        switch op {
+        case "+", "-":
+            return 1
+        case "x", "/", "%":
+            return 2
+        default:
+            return 0
+        }
+    }
+
+    func formErrorTest(_ equ: String) -> String? {
+        let regex = try! NSRegularExpression(pattern: pattern)
+        let range = NSRange(location: 0, length: equ.utf16.count)
+        
+        if regex.firstMatch(in: equ, options: [], range: range) != nil {
+            return equ
+        } else if equ.contains(" ") {
+            print("Invalid: Input contains spaces.")
+            exit(0)
+        } else if !equ.allSatisfy({ $0.isNumber || "+-x/%[]".contains($0) }) {
+            print("Invalid: Contains unsupported characters.")
+            exit(0)
+        } else if equ.hasPrefix("[") && equ.hasSuffix("]") {
+            print("Invalid: Cannot have both '[' and ']' at the same time.")
+            exit(0)
+        } else if equ.dropFirst().contains(where: { "+-x/%".contains($0) }) && equ.contains(where: { $0.isNumber }) {
+            print("Invalid: Cannot mix numbers and operators.")
+            exit(0)
+        } else if "+/%".contains(equ.first!) {
+            print("Invalid: '\(equ.first!)' cannot be at the start.")
+            exit(0)
+        } else if equ.filter({ $0 == "-" }).count > 1 && !equ.hasPrefix("[-") && !equ.hasPrefix("-") {
+            print("Invalid: Misplaced '-' detected.")
+            exit(0)
+        } else {
+            print("Invalid: Does not match any allowed format.")
+            exit(0)
+        }
+    }
+
+    func validEquTest(_ equ: [String]) -> [String] {
+        if let first = equ.first, let last = equ.last,
+        let _ = Double(first), let _ = Double(last) {
+            return equ
+        } else {
+            print("Invalid: Equation incomplete")
+            exit(0)
+        }
+    }
+
+    func tokenize(_ equ: [String]) -> [String] {
+        var token = validEquTest(equ)
+        var i = 0
+        while i < token.count {
+            if let test = formErrorTest(token[i]) {
+                if test.hasPrefix("[") && !test.hasSuffix("]") {
+                    token[i] = String(test.dropFirst())
+                    token.insert("[", at: i)
+                    i += 1
+                } else if test.hasSuffix("]") && !test.hasPrefix("[") {
+                    token[i] = String(test.dropLast())
+                    token.insert("]", at: i + 1)
+                    i += 2
+                } else {
+                    i += 1
+                }
+            }
+        }
+        return token
+    }
+
+    func shuntingYard(_ equ: [String]) -> [String] {
+        var equ = tokenize(equ)
+        var infix = equ
+        print("infix: " + infix.joined() + "\n")
+        var post: [String] = []
+        var op: [String] = []
+        var i = 0
+        while i < infix.count {
+            let token = infix[i]
+            
+            if let _ = Double(token) {
+                post.append(token)
+            } else if token == "[" {
+                op.append(token)
+            } else if token == "]" {
+                while let last = op.last, last != "[" {
+                    post.append(op.popLast()!)
+                }
+                op.popLast()
+            } else {
+                while let last = op.last, last != "[" && precedence(last) >= precedence(token) {
+                    post.append(op.popLast()!)
+                }
+                op.append(token)
+            }
+            
+            i += 1
+            print(" output: " + post.joined())
+            print(" stack: " + op.joined() + "\n")
+        }
+        
+        while let last = op.popLast() {
+            post.append(last)
+        }
+
+        print("postfix: " + post.joined() + "\n")
+        
+        return post
+    }
+
+    func evaluate(_ equ: [String]) -> Any {
+        let equ = shuntingYard(equ)
+        var eval: [Double] = []
+        var temp = 0.0
+        var i = 0
+        while i < equ.count {
+            if let number = Double(equ[i]) {
+                eval.append(number)
+            } else {
+                switch equ[i] {
+                case "+":
+                    temp = eval.removeLast(2).reduce(0, +)
+                    eval.append(temp)
+                case "-":
+                    temp = eval.removeLast(2).reduce(-)
+                    eval.append(temp)
+                case "x":
+                    temp = eval.removeLast(2).reduce(*)
+                    eval.append(temp)
+                case "/":
+                    temp = eval.removeLast(2).reduce(/)
+                    eval.append(temp)
+                case "%":
+                    temp = eval.removeLast(2).reduce(%)
+                    eval.append(temp)
+                default:
+                    break
+                }
+            }
+            i += 1
+        }
+
+        let result = eval.last!
+        if result.isInteger {
+            return Int(result)
+        } else {
+            return result
+        }
     }
 }
